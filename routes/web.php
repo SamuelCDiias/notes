d424e59d64bc0af98774bccdd0941d27f12116fb
Samuel Dias <samuelcarvalhodias2004@gmail.com>
2024-11-16 16:25:31 -0300

    022

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,17 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Middleware\CheckIsLogged;
+use App\Http\Middleware\CheckIsNotLogged;
 use Illuminate\Support\Facades\Route;
 
-// AUTH ROUTES
-route::get('/login', [AuthController::class, 'login']);
-route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+// AUTH ROUTES - USER NOT LOGGED
 
+route::middleware([CheckIsNotLogged::class])->group(function(){
+    route::get('/login', [AuthController::class, 'login']);
+    route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+});
+
+// AUTH ROUTES - USER LOGGED
 route::middleware([CheckIsLogged::class])->group(function(){
     route::get('/', [MainController::class, 'index']);
     route::get('/newNote', [MainController::class, 'newNote']);
