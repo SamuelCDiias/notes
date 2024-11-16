8d0e82d0536d5ea3b687020e844ff51d0b5eec34
Samuel Dias <samuelcarvalhodias2004@gmail.com>
2024-11-16 13:41:26 -0300

    012

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,9 +13,17 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo $request->input('text_username');
-        echo '<br>';
-        echo $request->input('text_password');
+        $request->validate(
+            [
+                'text_username' => 'required',
+                'text_password' => 'required'
+            ]
+        );
+
+        // get user input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+        echo 'OK!';
 
     }
 
