61295567e831b54d1eeda9acbfa7f280ba46526f
Samuel Dias <samuelcarvalhodias2004@gmail.com>
2024-11-16 11:42:54 -0300

    008

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\MainController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-
+route::get('/login', [AuthController::class, 'login']);
+route::get('/logout', [AuthController::class, 'logout']);
 
