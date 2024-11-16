96f66fef39ec875788e56dd89cee2bfd3bf26ef8
Samuel Dias <samuelcarvalhodias2004@gmail.com>
2024-11-16 19:13:20 -0300

    035

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -68,11 +68,44 @@ class MainController extends Controller
    }
 
 
-   public function editNoteSubmit()
+   public function editNoteSubmit(Request $request)
    {
+    // validated
+    $request->validate(
+        [
+            'text_title' => 'required|min:3|max:200',
+            'text_note' => 'required|min:3|max:3000'
+        ],
+        [
+            'text_title.required' => 'O título precisa ser preenchido',
+            'text_title.min' => 'O título precisa ter no mínimo :min caracteres',
+            'text_title.max' => 'O título precisa ter no máximo :max caracteres',
+            'text_note.required' => 'A nota precisa ser preenchida',
+            'text_note.min' => 'A nota precisa ter no mínimo :min caracteres',
+            'text_note.max' => 'A nota precisa ter no máximo :max caracteres'
+        ]
+        );
 
+    // check if note_id exist
+
+    if($request->note_id == NULL){
+        return redirect()->route('home');
+    }
+
+    // decrypt note_id
+    $id = Operations::decryptId($request->note_id);
+
+
+    // load note
+    $note = Note::find($id);
+
+    // update note
+    $note->title = $request->text_title;
+    $note->text = $request->text_note;
+    $note->save();
+
+    // return home
     return redirect()->route('home');
-
    }
 
    public function deleteNote($id)
