6c0499e8991c98ca59c29d79e61d2aa80e7b2349
Samuel Dias <samuelcarvalhodias2004@gmail.com>
2024-11-16 18:50:50 -0300

    033

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
@@ -27,7 +27,33 @@ class MainController extends Controller
 
    public function newNoteSubmit(Request $request)
    {
-    echo "I'm creating a new note";
+    // validate request
+    $request->validate(
+        [
+            'text_title' => 'required|min:3|max:200',
+            'text_note' => 'required|min:3|max:3000'
+        ],
+        [
+            'text_title.required' => 'O título precisa ser preenchido',
+            'text_title.min' => 'O título precisa ter no mínimo :min caracteres',
+            'text_title.max' => 'O título precisa ter no máximo :max caracteres',
+            'text_note.required' => 'A nota precisa ser preenchida',
+            'text_note.min' => 'A nota precisa ter no mínimo :min caracteres',
+            'text_note.max' => 'A nota precisa ter no máximo :max caracteres'
+        ]
+        );
+    //get user id
+    $id = session('user.id');
+
+    //create new note
+    $note = new Note();
+    $note->user_id = $id;
+    $note->title = $request->text_title;
+    $note->text = $request->text_note;
+    $note->save();
+
+    //redirect to home
+    return redirect()->route('home');
    }
 
    public function editNote($id)
