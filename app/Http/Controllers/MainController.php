3fec727de7f1f4a3ccbfc182bf5d06052a1a8f08
Samuel Dias <samuelcarvalhodias2004@gmail.com>
2024-11-16 18:18:06 -0300

    031

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Models\User;
-use Illuminate\Contracts\Encryption\DecryptException;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Crypt;
+use App\Services\Operations;
 
 class MainController extends Controller
 {
@@ -15,8 +14,6 @@ class MainController extends Controller
 
     $id = session('user.id');
     $notes = User::find($id)->notes()->get()->toArray();
-
-
 
     // load home view
     return view('home', ['notes' => $notes]);
@@ -29,25 +26,15 @@ class MainController extends Controller
 
    public function editNote($id)
    {
-    $id = $this->decryptId($id);
+    $id = Operations::decryptId($id);
     echo "Hello! I'm editing note with id = $id";
 
    }
 
    public function deleteNote($id)
    {
-    $id = $this->decryptId($id);
+    $id = Operations::decryptId($id);
     echo "Hello! I'm deleting note with id = $id";
-   }
-
-   private function decryptId($id)
-   {
-    try {
-        $id = Crypt::decrypt($id);
-    } catch (DecryptException $e) {
-        return redirect()->route('home');
     }
-
-    return $id;
    }
-}
+
